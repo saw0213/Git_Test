dea052293945b04c4da5d51642f392c94be8f6fe
USER <USER@test.com>
2018-04-04 10:40:55 +0900

    .

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -3,6 +3,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hi there!");
+		System.out.println("It's YeongJae!!");
+		System.out.println("Glad to meet you :)");
 
 	}
 
